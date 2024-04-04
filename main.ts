b88af90edbd4d7be32f2ed3c3e92ed62e7147743
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-04-04 12:10:39 +0800

    update

--- main.ts
+++ main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/modules/app.module';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(bodyParser.json());
   await app.listen(3000);
 }
 bootstrap();
