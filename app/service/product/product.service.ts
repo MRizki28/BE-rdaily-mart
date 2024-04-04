f60c52d6832d4c526df946c383c748535e9fc7ec
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-04-04 12:13:32 +0800

    create new function

--- app/service/product/product.service.ts
+++ app/service/product/product.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
 import { ProductModel } from '../../models/product.model';
 import { HttpResponseTraits } from '../../Traits/HttpResponseTraits';
+import { ProductRequest } from '../../Request/product/product.request';
+import * as Joi from 'joi';
+import { createWriteStream } from 'fs';
 
 @Injectable()
 export class ProductService {
@@ -15,12 +18,41 @@ export class ProductService {
             const data = await this.productModel.findAll();
             if (!data || data.length == 0) {
                 return HttpResponseTraits.dataNotFound()
-            }else{
+            } else {
                 return HttpResponseTraits.success(data)
             }
         } catch (error) {
             console.error('Error while fetching products:', error);
-            throw error; 
+            throw error;
+        }
+    }
+
+    async createData(productData: any, productImage: any): Promise<any> {
+        try {
+            const { product_name, stok } = productData;
+            const { error } = ProductRequest.validate({ product_name, stok, product_image: productImage });
+            
+            if (error) {
+                const errors = [error.message];
+                return HttpResponseTraits.checkValidation(errors);
+            }
+            const filename = productImage.originalname;
+            const uploadPath = `./assets/uploads/product/${filename}`;
+            const writeStream = createWriteStream(uploadPath);
+            writeStream.write(productImage.buffer);
+
+            const data = await this.productModel.create({
+                product_name,
+                stok,
+                product_image: filename 
+            });
+
+            return HttpResponseTraits.success(data);
+        } catch (error) {
+            console.log(error);
+            return {
+                message: 'failed'
+            };
         }
     }
 }
