244eafdfbb6ba5e5e9e978794ced5cdb491babb2
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-04-08 13:33:24 +0800

    create update function

--- app/service/product/product.service.ts
+++ app/service/product/product.service.ts
@@ -6,6 +6,8 @@ import { ProductRequest } from '../../request/product/product.request';
 import { createWriteStream } from 'fs';
 import { unlink } from 'fs/promises';
 import { v4 as uuidv4 } from 'uuid';
+import * as fs from 'fs';
+
 
 @Injectable()
 export class ProductService {
@@ -21,25 +23,25 @@ export class ProductService {
                 limit: pageSize,
                 offset: offset
             });
-    
+
             if (!data || data.length == 0) {
                 return HttpResponseTraits.dataNotFound();
             } else {
                 const totalCount = await this.productModel.count();
                 const totalPages = Math.ceil(totalCount / pageSize);
-    
+
                 let nextUrl = null;
                 if (page < totalPages) {
                     const nextPage = Math.min(page + 1, totalPages);
                     nextUrl = `product?page=${nextPage}`;
                 }
-    
+
                 let prevUrl = null;
                 if (page > 1) {
                     const prevPage = page - 1;
                     prevUrl = `product?page=${prevPage}`;
                 }
-    
+
                 return HttpResponseTraits.success({
                     data: data,
                     currentPage: page,
@@ -55,12 +57,26 @@ export class ProductService {
             throw error;
         }
     }
-    
+
+    async getDataById(id: string): Promise<any> {
+        try {
+            const data = await this.productModel.findByPk(id)
+            if (!data) {
+                return HttpResponseTraits.idOrDataNotFound()
+            } else {
+                return HttpResponseTraits.success(data)
+            }
+        } catch (error) {
+            console.log(error);
+        };
+
+    }
+
     async createData(productData: any, productImage: any): Promise<any> {
         try {
             const { product_name, stok, price } = productData;
             const { error } = ProductRequest.validate({ product_name, price, stok, product_image: productImage });
-            
+
             if (error) {
                 const errors = [error.message];
                 return HttpResponseTraits.checkValidation(errors);
@@ -74,7 +90,7 @@ export class ProductService {
                 product_name,
                 price,
                 stok,
-                product_image: filename 
+                product_image: filename
             });
 
             return HttpResponseTraits.success(data);
@@ -86,6 +102,44 @@ export class ProductService {
         }
     }
 
+    async updateData(productData: any, productImage: any, id: string): Promise<any> {
+        try {
+            const { product_name, stok, price } = productData
+            let updateData
+            if (productImage) {
+                const filename = `${uuidv4()}-${productImage.originalname}`;
+                const uploadPath = `./assets/uploads/product/${filename}`;
+                const writeStream = createWriteStream(uploadPath);
+                writeStream.write(productImage.buffer);
+
+                updateData = await this.productModel.findByPk(id);
+                if (updateData) {
+                    if (updateData.product_image) {
+                        await unlink(`./assets/uploads/product/${updateData.product_image}`);
+                    }
+                    updateData.product_image = product_name
+                    updateData.stok = stok
+                    updateData.price = price
+                    updateData.product_image = filename
+                    await updateData.save();
+                }
+            } else {
+                updateData = await this.productModel.findByPk(id);
+                if (updateData) {
+                    updateData.product_name = product_name;
+                    updateData.price = price;
+                    updateData.stok = stok;
+                    await updateData.save();
+                }
+            }
+
+            return HttpResponseTraits.success(updateData)
+        } catch (error) {
+            console.log(error);
+        };
+
+    }
+
     async deleteData(id: string): Promise<any> {
         try {
             const data = await this.productModel.findByPk(id)
@@ -95,7 +149,7 @@ export class ProductService {
 
             console.log(data.product_image);
             const imagePath = "./assets/uploads/product/" + data.product_image;
-            
+
             await unlink(imagePath)
 
             await data.destroy()
@@ -104,6 +158,24 @@ export class ProductService {
         } catch (error) {
             console.log(error);
         };
-        
+
+    }
+
+    async getGambar(product_image: string): Promise<Buffer | null> {
+        try {
+            const product = await this.productModel.findOne({
+                where: { product_image },
+            });
+
+            if (!product) {
+                throw new Error('Image not found');
+            }
+            const imagePath = `./assets/uploads/product/${product.product_image}`;
+            const imageBuffer = fs.readFileSync(imagePath);
+            return imageBuffer;
+        } catch (error) {
+            console.error(error);
+            return null;
+        }
     }
 }
