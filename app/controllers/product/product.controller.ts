fb2d51913119a391b8e171092db89e6acc1f05f7
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-04-08 13:33:16 +0800

    create update function

--- app/controllers/product/product.controller.ts
+++ app/controllers/product/product.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Delete, Get, Param, Post, Query, Req, UploadedFile, UseInterceptors, UsePipes } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Query, Req, Res, UploadedFile, UseInterceptors, UsePipes } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { ProductService } from 'app/service/product/product.service';
+import { Response } from 'express';
 
 @Controller('product')
 export class ProductController {
@@ -33,6 +34,27 @@ export class ProductController {
         }
     }
 
+    @Post('update/:id')
+    @UseInterceptors(FileInterceptor('product_image'))
+    async updateData(@Param('id') id: string, @UploadedFile() productImage, @Body() productData): Promise<any> {
+        try {
+            const data = await this.productService.updateData(productData, productImage, id);
+            return data;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    @Get('get/:id')
+    async getDataById(@Param('id') id: string): Promise<any> {
+        try {
+            const data = await this.productService.getDataById(id)
+            return data
+        } catch (error) {
+            console.log(error);
+        };
+    }
+
     @Delete('delete/:id')
     async deleteData(@Param('id') id: string): Promise<any> {
         try {
@@ -41,5 +63,19 @@ export class ProductController {
         } catch (error) {
             console.log(error);
         };
+    }
+
+    @Get('image/:product_image')
+    async getProductImage(@Param('product_image') productImage: string, @Res() res: Response): Promise<void> {
+        const imageBuffer = await this.productService.getGambar(productImage);
+        if (imageBuffer) {
+            res.setHeader('Content-Type', 'image/jpeg'); 
+            res.send(imageBuffer);
+        } else {
+            res.status(404).json({
+                code: 404,
+                message: 'Image not found',
+            });
+        }
     }
 }
