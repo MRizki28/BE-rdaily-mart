ae02d6b5fc178a833e8b658a9ef109d2b6cfacbb
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-04-19 16:52:52 +0800

    create function for get data and create data

--- app/modules/typeproduct/typeProduct.module.ts
+++ app/modules/typeproduct/typeProduct.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { TypeProductModel } from '../../models/type_product.model'
 import { TypeProductService } from '../../service/typeproduct/typeProduct.service'
+import { TypeProductController } from '../../controllers/typeproduct/typeproduct.controller'
 @Module({
     imports: [SequelizeModule.forFeature([TypeProductModel])],
     providers: [TypeProductService],
+    controllers: [TypeProductController]
 })
 export class TypeProductModule { }
