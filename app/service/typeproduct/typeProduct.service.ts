8d60780eee574aaabe1b93bed674dd0c591f9334
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-04-23 21:08:47 +0800

    create get data by id and update data

--- app/service/typeproduct/typeProduct.service.ts
+++ app/service/typeproduct/typeProduct.service.ts
@@ -8,47 +8,47 @@ export class TypeProductService {
     constructor(
         @InjectModel(TypeProductModel)
         private readonly typeProductModel: typeof TypeProductModel,
-    ){}
+    ) { }
 
-    async getAllData(page: number= 1, pageSize: number = 0 ): Promise<any> {
+    async getAllData(page: number = 1, pageSize: number = 0): Promise<any> {
         try {
-        const offset = (page -1) * pageSize
-        const data = await this.typeProductModel.findAll({
-            limit: pageSize,
-            offset: offset
-        })
+            const offset = (page - 1) * pageSize
+            const data = await this.typeProductModel.findAll({
+                limit: pageSize,
+                offset: offset
+            })
 
-    if (!data || data.length == 0) {
-            return HttpResponseTraits.dataNotFound()
-        }else{
-            const totalCount = await this.typeProductModel.count()
-            const totalPages = Math.ceil(totalCount / pageSize)
-            
-            let nextUrl = null
+            if (!data || data.length == 0) {
+                return HttpResponseTraits.dataNotFound()
+            } else {
+                const totalCount = await this.typeProductModel.count()
+                const totalPages = Math.ceil(totalCount / pageSize)
 
-            if (page < totalPages) {
-                const nextPage = Math.min(page + 1 , totalPages)
-                nextUrl = `typeproduct=${nextPage}`
+                let nextUrl = null
+
+                if (page < totalPages) {
+                    const nextPage = Math.min(page + 1, totalPages)
+                    nextUrl = `typeproduct=${nextPage}`
+                }
+
+                let prevUrl = null
+
+                if (page > 1) {
+                    const prevPage = page - 1
+                    nextUrl = `typeproduct=${prevPage}`
+                }
+
+                return HttpResponseTraits.success({
+                    data: data,
+                    currentPage: page,
+                    totalPages: totalPages,
+                    pageSize: pageSize,
+                    totalCount: totalCount,
+                    nextPage: nextUrl,
+                    prevPage: prevUrl
+                });
+
             }
-
-            let prevUrl = null
-
-            if (page > 1) {
-                const prevPage = page - 1
-                nextUrl = `typeproduct=${prevPage}`
-            }
-
-            return HttpResponseTraits.success({
-                data: data,
-                currentPage: page,
-                totalPages: totalPages,
-                pageSize: pageSize,
-                totalCount: totalCount,
-                nextPage: nextUrl,
-                prevPage: prevUrl
-            });
-
-        }
         } catch (error) {
             console.log(error);
         };
@@ -76,6 +76,37 @@ export class TypeProductService {
                 message: 'failed'
             }
         };
-        
+
+    }
+
+    async getDataById(id: string): Promise<any> {
+        try {
+            const data = await this.typeProductModel.findByPk(id)
+            return (!data) ? HttpResponseTraits.idOrDataNotFound() : HttpResponseTraits.success(data)
+        } catch (error) {
+            console.log(error);
+        };
+    }
+
+    async updateData(typeProductData: any, id: string): Promise<any> {
+        try {
+            const data = await this.typeProductModel.findByPk(id)
+            if (!data) {
+                HttpResponseTraits.idOrDataNotFound()
+            } else {
+                const { type_product } = typeProductData;
+                const { error } = TypePoductRequest.validate({
+                    type_product
+                })
+
+                return error ? HttpResponseTraits.checkValidation([error.message]) : (() => {
+                    data.type_product = type_product
+                    data.save()
+                    return HttpResponseTraits.success(data)
+                })();
+            }
+        } catch (error) {
+            console.log(error);
+        };
     }
 }
