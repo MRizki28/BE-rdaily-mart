f010ec001edccd492033f92fe6ba5afef13326cf
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-05-18 16:26:24 +0800

    fix(update):fix update img

--- app/request/product/product.request.ts
+++ app/request/product/product.request.ts
@@ -1,6 +1,7 @@
 import * as Joi from 'joi';
 
 export const ProductRequest = Joi.object({
+    id_type_product: Joi.required(),
     product_name: Joi.string().required(),
     price: Joi.required(),
     stok: Joi.required(),
