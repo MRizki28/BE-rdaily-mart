744167d7972ab17e3853ec2d3894b26bac9bc714
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-05-20 19:37:04 +0800

    feat(authentication): create register and login

---
+++ app/modules/user/user.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { UserModel } from '../../models/user.model'
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { UserService } from '../../service/user/user.service'
+import { UserController } from '../../controllers/user/user.controller'
+import { JwtStrategy } from '../../auth/jwt.strategy'
+require('dotenv').config();
+
+@Module({
+    imports: [
+        SequelizeModule.forFeature([UserModel]),
+        PassportModule,
+        JwtModule.register({
+            secret: process.env.SECRET_KEY,
+            signOptions: { expiresIn: '60s' }
+        }),
+    ],
+    providers: [UserService, JwtStrategy],
+    controllers: [UserController]
+
+})
+export class UserModule { }
