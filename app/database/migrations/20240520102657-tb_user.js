1f9a65131934b96b35e904c4f3a570cca197298d
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-05-20 19:36:50 +0800

    feat(authentication): create register and login

---
+++ app/database/migrations/20240520102657-tb_user.js
@@ -0,0 +1,47 @@
+'use strict';
+const sequelize = require('sequelize');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('tb_user', {
+      id: {
+        type: Sequelize.UUID,
+        defaultValue: sequelize.UUIDV4,
+        allowNull: false,
+        primaryKey: true
+      },
+
+      username: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+
+      access_token: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+
+      updated_at: {
+        field: "updated_at",
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+
+    })
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('tb_user')
+  }
+};
